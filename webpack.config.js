747c75267fc33f6d4c9c8d96f690dbeba5f0a778
Trung Vo <trung.vo@zyllem.com>
2020-06-15 09:55:55 +0800

    Add purgecss

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,13 @@ module.exports = {
             require("postcss-import"),
             require("tailwindcss"),
             require("autoprefixer"),
+            require("@fullhuman/postcss-purgecss")({
+              // Specify the paths to all of the template files in your project
+              content: [
+                "./src/**/*.html",
+                "./src/**/*.ts"                
+              ],              
+            })
           ],
         },
       },
